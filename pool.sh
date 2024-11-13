2c1b8f4a26cd51cf1888c563ac86f2fd79f49e64
AdrianCE94 <adriance4jobby@gmail.com>
2024-11-13 17:07:18 +0100

    Improve pool.sh script with enhanced user prompts and error handling

--- pool.sh
+++ pool.sh
@@ -12,12 +12,14 @@ comprobarKVM () {
         exit 1
     else
         echo "KVM está instalado"
+        echo "====================="
     fi
 }
 comprobarRoot () {
     existe=$(id -u)
     if [ $existe -eq 0 ]; then
-        echo "Puedes continuar"
+        echo "Puedes continuar,eres ROOT"
+        echo "================================"
     else
         echo "Debes ser Root para ejecutar este script"
         exit 1
@@ -25,14 +27,15 @@ comprobarRoot () {
 }
 menu(){
 
-    echo "1.Visualizar los pools existentes"
-    echo "2.Crear pool"
-    echo "3.Creacion de discos duros"
-    echo "4.Mostrar información de los discos"
+    echo "1.Visualizar los pools existentes "
+    echo "2.Crear pool "
+    echo "3.Creacion de discos duros "
+    echo "4.Mostrar información de los discos "
     echo "5.Agregar discos a máquina virtual"
-    echo "6.Definir pools con arranque automático"    
-    echo "7.Borrado pool"
-    echo "8.Generar archivo de pools creados"
+    echo "6.Definir pools con arranque automático "    
+    echo "7.Borrado pool "
+    echo "8.Generar archivo de pools creados "
+    echo "9.salir"
 }
 
 option1(){
@@ -43,8 +46,8 @@ option2(){
 
     read -p "Introduce el nombre del pool: " nombrePool
     echo "Creando pool $nombrePool...."
-    mkdir /var/lib/libvirt/images/$nombrePool
-    virsh pool-define-as $nombrePool --type dir --target /var/lib/libvirt/images/$nombrePool
+    mkdir /var/lib/libvirt/$nombrePool
+    virsh pool-define-as $nombrePool --type dir --target /var/lib/libvirt/$nombrePool
     virsh pool-start $nombrePool
 }
 
@@ -53,33 +56,35 @@ option3(){
     # se le pedira al usuario el numero de discos  y el nombre
     read -p "Introduce el numero de discos duros que quieres crear: " numDiscos
     read -p "Escribe el nombre del pool: " nPool
-    for ((i=1; i<=$numDiscos; i++)); do
+    for i in $(seq 1 "$numDiscos"); do
         read -p "Introduce el nombre del disco duro $i: " nombreDisco
-        read -p "Introduce el tamaño del disco duro $i: " disco
-        virsh vol-create-as --pool $nPool --format qcow2 $disco.qcow2 $tamanoDisco
+        read -p "Introduce el tamaño del disco duro $i (en GB): " tamanoDisco
+        virsh vol-create-as --pool "$nPool" --format qcow2 "$nombreDisco".qcow2 "$tamanoDisco"G
     done
 }
 
 option4(){
     read -p "Introduce el nombre del pool para ver los volumenes: " miPool
     echo "Refrescando pool $miPool...."
-    virsh refresh $miPool
+    virsh pool-refresh $miPool
     echo "Mostrando información de los discos duros del pool $miPool...."
     virsh vol-list $miPool --details
 }
 
 option5(){
     #ver discos disponibles
-    read -p "Introduce el nombre del pool" namePool
+    #pool disponibles
+    virsh pool-list --all
+    read -p "Introduce el nombre del pool: " namePool
     virsh vol-list $namePool --details
     read -p "Introduce el nombre de la máquina virtual: " nombreMV
-    read -p "Introduce el nombre del disco a agregar: " nombreDisco
-    virsh attach-disk $nombreMV --source /var/lib/libvirt/images/$nombreDisco.qcow2  vdb --persistent --subdriver qcow2
+    read -p "Introduce el nombre del disco a agregar(sin la extension): " nombreDisco
+    virsh attach-disk "$nombreMV" --source /var/lib/libvirt/$namePool/$nombreDisco.qcow2  vdb --persistent --subdriver qcow2
 }
 
 option6(){
     read -p "Introduce el nombre del pool para arranque automático: " poool
-    virsh pool-autostart $pool
+    virsh pool-autostart "$poool"
 }
 
 option7(){
@@ -89,7 +94,7 @@ option7(){
     read -p "Introduce el nombre del pool a borrar: " pool
     virsh pool-destroy $pool
     virsh pool-undefine $pool
-    rm -r /var/lib/libvirt/images/$pool
+    rm -r /var/lib/libvirt/$pool
 }
 
 option8(){
@@ -104,7 +109,7 @@ comprobarRoot
 comprobarKVM
 menu
 echo ""
-read -p "Introduce una opción del menú: " opcion
+read -p "Introduce una opción del menú : " opcion
 while [ "$opcion" != 9 ]; do
     case "$opcion" in
         1)
@@ -135,9 +140,11 @@ while [ "$opcion" != 9 ]; do
             echo "Opción no válida"
             ;;
     esac
-    read -p "INTRO PARA CONTINUAR" INTRO
+    
+    read -p "INTRO PARA CONTINUAR " INTRO
+    echo " "
     menu
-    read -p "Introduce una opción del menú: " opcion
+    echo " "
+    read -p "Introduce una opción del menú : " opcion
+    
 done
-
-
